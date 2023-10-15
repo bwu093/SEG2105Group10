38243cb037ab6f35c17d7818b4f94f5e206bd763
kasperfuro <kasper.furo@gmail.com>
2023-10-15 13:18:46 -0400

    added user class

---
+++ SEGProject/app/src/main/java/com/example/segproject/User.java
@@ -0,0 +1,17 @@
+
+//this class represents a user
+public class User{
+
+    //instance variables
+    protected int userID;
+    protected String firstName;
+    protected String lastName;
+    protected String emailUsername;
+    protected String password;
+    protected String phoneNumber;
+    protected Address address;
+
+
+    //constructors
+    public User()
+}
