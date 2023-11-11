4eb69d8d91c5aaacbed27b9c0abc58a604aae152
AymanAljanabi <145711291+AymanAljanabi@users.noreply.github.com>
2023-11-11 17:11:05 -0500

    Added variables representing past appoitnments, upcoming appointments, and upcoming shifts for the doctor. Also added a method to retrieve information for the patient to be viewed by the doctor.

--- SEGProject/app/src/main/java/com/example/segproject/Doctor.java
+++ SEGProject/app/src/main/java/com/example/segproject/Doctor.java
@@ -1,39 +1,104 @@
 package com.example.segproject;
 
 import java.util.ArrayList;
+import java.util.List;
 
 //this class represents the doctor which extends user
-public class Doctor extends User{
+public class Doctor extends User {
 
     //instance variables
     protected String employeeNumber;
 
     protected String specialties;
 
+    protected List<App> pastAppointments;
+
+    protected List<App> upcomingAppointments;
+
+    protected List<Sft> upcomingShiftsForDoctor;
+
     //constructor
-    public Doctor(String userId, String username, String password, String firstName, String lastName, String phoneNumber, String address, String employeeNumber, String specialties){
+    public Doctor(String userId, String username, String password, String firstName, String lastName, String phoneNumber, String address, String employeeNumber, String specialties) {
         super(userId, username, password, firstName, lastName, phoneNumber, address, "Doctor");
         this.employeeNumber = employeeNumber;
         this.specialties = specialties;
+        this.pastAppointments = new ArrayList<>();
+        this.upcomingAppointments = new ArrayList<>();
+        this.upcomingShiftsForDoctor = new ArrayList<>();
+
+
     }
+    // Getters and Setters
 
     public String getEmployeeNumber() {
+
         return employeeNumber;
     }
+
     public void setEmployeeNumber(String employeeNumber) {
+
         this.employeeNumber = employeeNumber;
     }
 
     public String getSpecialties() {
+
         return specialties;
     }
-    // Setter for healthCardNumber
-    public void setSpecialties(String healthCardNumber) {
-        this.specialties = healthCardNumber;
+
+    public void setSpecialties(String specialties) {
+
+        this.specialties = specialties;
     }
 
-    //methods
+    public List<App> getPastAppointments() {
+        return pastAppointments;
+    }
 
+    public void setPastAppointments(List<App> pastAppointments) {
+        this.pastAppointments = pastAppointments;
+    }
 
+    public List<App> getUpcomingAppointments() {
+        return upcomingAppointments;
+    }
 
+    public void setUpcomingAppointments(List<App> upcomingAppointments) {
+        this.upcomingAppointments = upcomingAppointments;
+    }
+
+    public List<Sft> getUpcomingShiftsForDoctor() {
+        return upcomingShiftsForDoctor;
+    }
+
+    public void setUpcomingShiftsForDoctor(List<Sft> upcomingShiftsForDoctor) {
+        this.upcomingShiftsForDoctor = upcomingShiftsForDoctor;
+    }
+
+    // Prints the information of patient including first name, last name, username, password, phone number, address, and health card number
+    public void informationOfGivenPatient(Appoint appoint) {
+        if (appoint == null) {
+            System.out.println("There is no given informaton on the appointment for the patient");
+        }
+        Pat pat = appoint.getPatient();
+        if (pat != null) {
+            System.out.println("Information about patient:" +
+                    "\n" + "First Name:" + pat.getFirstName() +
+                    "\n" + "Last Name:" + pat.getLastName() +
+                    "\n" + "Username" + pat.getUsername() +
+                    "\n" + "Password:" + pat.getPasword() +
+                    "\n" + "Phone Number:" + pat.getPhoneNumber() +
+                    "\n" + "Address" + pat.getAddress() +
+                    "\n" + "Health Card Number:" + pat.getHealthCardNumber());
+        } else if (pat == null) {
+            System.out.println("There is no given information for the patient");
+        }
+    }
 }
+
+
+
+
+
+
+
+
