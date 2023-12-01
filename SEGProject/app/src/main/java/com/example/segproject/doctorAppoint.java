15ed52ff20a7b9d71bd7f8617aca648650254d65
bswu2 <bwu093@uottawa.ca>
2023-12-01 13:32:33 -0500

    small fix

---
+++ SEGProject/app/src/main/java/com/example/segproject/doctorAppoint.java
@@ -0,0 +1,28 @@
+package com.example.segproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+public class doctorAppoint extends AppCompatActivity {
+}
